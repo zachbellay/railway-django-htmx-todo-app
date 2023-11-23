b9e4517393503d802130efa7f2e0880400fea90e
Zach Bellay <zachbellay@hotmail.com>
2023-11-23 13:54:49 -0800

    add todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.description
