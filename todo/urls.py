7da961e742198f5ea2539caf1b818973d767d8b3
Zach Bellay <zachbellay@hotmail.com>
2023-11-23 13:33:42 -0800

    add home and todo templates and add ruff

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-from .views import home
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
