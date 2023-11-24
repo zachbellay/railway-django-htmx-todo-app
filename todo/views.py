dc9bed958cb2a3fd8d6715bf1d66ce9288e0984b
Zach Bellay <zachbellay@hotmail.com>
2023-11-24 11:44:11 -0800

    add method requirements

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_GET, require_http_methods, require_POST
 
 from todo.models import Todo
 
 
+@require_GET
 def todo(request):
     context = {"todos": Todo.objects.all()}
     return render(request, "todo.html", context)
 
 
+@require_POST
 def add_todo(request):
     if request.htmx:
         todo = Todo.objects.create(
@@ -19,6 +22,7 @@ def add_todo(request):
         return render(request, "partials/todo.html", {"todo": todo})
 
 
+@require_http_methods(["DELETE"])
 def delete_todo(request, pk):
     if request.htmx:
         Todo.objects.get(pk=pk).delete()
