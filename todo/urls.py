487cdb869f473075131b9cb61a2c9716dbf2a3ce
Zach Bellay <zachbellay@hotmail.com>
2023-11-23 17:43:45 -0800

    ugly first pass

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="todo.html"), name="home"),
+    path("", views.todo, name="todo"),
 ]
 
 
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from todo.models import Todo
+
+
+def todo(request):
+    context = {
+        "todos": Todo.objects.all(),
+    }
+    return render(request, "todo.html", context)
 
 
 def add_todo(request):
-    return HttpResponse("added todo")
+    if request.htmx:
+        todo = Todo.objects.create(
+            description=request.POST.get("todo"),
+            completed=False,
+        )
+
+        return HttpResponse(
+            f"<li id='todo-{todo.id}'><input type='checkbox' name='todo-{todo.id}'><span>{todo.description}</span></li>"
+        )
